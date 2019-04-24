0f92ef4fb3708273beb115914e82272b4434048b
Kernelgarden <kernelgarden@gmail.com>
2019-04-24 23:47:23 +0900

    add queue

--- lib/ex_rudp/message.ex
+++ lib/ex_rudp/message.ex
@@ -1,3 +1,3 @@
 defmodule ExRudp.Message do
-  defstruct next: nil, buf: <<>>, id: 0, tick: 0
+  defstruct buf: <<>>, id: 0, tick: 0
 end
--- lib/ex_rudp/message/queue.ex
+++ lib/ex_rudp/message/queue.ex
@@ -1,12 +1,42 @@
 defmodule ExRudp.Message.Queue do
-
   alias ExRudp.Message
 
   defstruct internal_queue: [], num: 0
 
-  @spec pop(__MODULE__.t(), integer()) :: {:ok, {__MODULE__.t(), Message.t()}}
+  @spec pop(__MODULE__.t(), integer()) ::
+          {:ok, {__MODULE__.t(), Message.t()}}
+          | {:error, :empty}
+          | {:error, :not_matched}
   def pop(queue, id) do
-    queue
+    case Enum.at(queue.internal_queue, -1) do
+      nil ->
+        nil
+
+      message ->
+        do_pop(queue, id, message)
+    end
+  end
+
+  defp do_pop(_queue, _id, message) when message == nil do
+    {:error, :empty}
+  end
+
+  defp do_pop(queue, id, message) do
+    case message.id >= 0 and message.id != id do
+      true ->
+        {:error, :not_matched}
+
+      false ->
+        queue = put_in(queue.num, queue.num - 1)
+
+        queue =
+          put_in(
+            queue.internal_queue,
+            List.delete_at(queue.internal_queue, -1)
+          )
+
+        {:ok, {queue, message}}
+    end
   end
 
   @spec push(__MODULE__.t(), Message.t()) :: __MODULE__.t()
@@ -15,5 +45,4 @@ defmodule ExRudp.Message.Queue do
     queue = put_in(queue.num, queue.num + 1)
     queue
   end
-
 end
